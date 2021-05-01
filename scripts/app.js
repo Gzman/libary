03e975cd2e01f3ab5d4f069e4d40123e1c12a9d5
Gzman <ganzmann@protonmail.com>
2021-05-01 18:57:46 +0200

    Implemented "Add book"-modal-window

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,16 @@
 import { Book, myLibary } from "./libary.js";
 
 const table = document.querySelector("tbody");
+const titleField = document.querySelector("#book-title");
+const authorField = document.querySelector("#book-author");
+const pagesField = document.querySelector("#book-pages");
+const publishedField = document.querySelector("#book-published");
+const statusField = document.querySelector("#book-status");
 
-function displayBooks() {
+const cleanTable = () => table.textContent = "";
+
+function renderBooks() {
+    cleanTable();
     myLibary.forEach((book) => {
         const tableRow = document.createElement("tr");
         tableRow.id = myLibary.indexOf(book);
@@ -19,19 +27,111 @@ function displayBooks() {
     document.querySelectorAll(".remove-btn").forEach(btn => btn.addEventListener("click", removeClicked));
 }
 
+function addBookToLibary() {
+    const title = titleField.value;
+    const author = authorField.value;
+    const pages = pagesField.value.length > 0 ? parseInt(pagesField.value) : "";
+    const published = publishedField.value.length > 0 ? parseInt(publishedField.value) : "";
+    const status = statusField.checked;
+    myLibary.push(new Book(
+        title,
+        author,
+        published,
+        pages,
+        status
+    ));
+}
+
 function removeClicked() {
     const tableRow = this.parentElement.parentElement;
     const bookId = parseInt(tableRow.id);
     myLibary.splice(bookId, 1);
-    table.textContent = "";
-    displayBooks();
+    renderBooks();
 }
 
-function statusClicked(event) {
-    const checkbox = event.target;
-    const tr = checkbox.parentElement.parentElement;
-    const bookId = parseInt(tr.id);
-    myLibary[bookId].haveRead = checkbox.checked;
+function statusClicked() {
+    const tableRow = this.parentElement.parentElement;
+    const bookId = parseInt(tableRow.id);
+    myLibary[bookId].haveRead = this.checked;
 }
 
-displayBooks();
+function resetInput() {
+    titleField.value = "";
+    authorField.value = "";
+    pagesField.value = "";
+    publishedField.value = "";
+    titleField.placeholder = "Title";
+    authorField.placeholder = "Author";
+    pagesField.placeholder = "Number of pages";
+    publishedField.placeholder = "YYYY";
+    statusField.checked = false;
+}
+
+function renderErrorMessage(textField, error) {
+    textField.value = "";
+    textField.placeholder = error;
+}
+
+function isValidTitle() {
+    if (titleField.value.length <= 0) {
+        renderErrorMessage(titleField, "Please enter a title");
+        return false;
+    }
+    return true;
+}
+
+function isValidPageNumber() {
+    const pages = pagesField.value;
+    const pageNumber = parseInt(pages);
+    if (pages.length > 0 && (Number.isNaN(pageNumber) || pageNumber <= 0)) {
+        renderErrorMessage(pagesField, "Enter a number greater than 0");
+        return false;
+    }
+    return true;
+}
+
+function isValidReleaseyear() {
+    if (publishedField.value.length > 0) {
+        if (publishedField.value.length !== 4) {
+            renderErrorMessage(publishedField, "Enter a year in the format: YYYY");
+            return false;
+        }
+        const year = parseInt(parseInt(publishedField.value));
+        if (Number.isNaN(year)) {
+            renderErrorMessage(publishedField, "Only numbers are allowed for release year");
+            return false;
+        }
+        const currentYear = new Date().getFullYear();
+        if (year > currentYear) {
+            renderErrorMessage(publishedField, "Release year can't be set in the future");
+            return false;
+        }
+    }
+    return true;
+}
+
+function isValidInput() {
+    return isValidTitle() && isValidPageNumber() && isValidReleaseyear();
+}
+
+function addBookClicked() {
+    document.querySelector(".modal-window").style["display"] = "block";
+}
+
+function closeClicked() {
+    resetInput();
+    document.querySelector(".modal-window").style["display"] = "none";
+}
+
+function submitClicked() {
+    if (!isValidInput()) return;
+    addBookToLibary();
+    resetInput();
+    renderBooks();
+}
+
+document.querySelector(".add-book-btn").addEventListener("click", addBookClicked);
+document.querySelector(".close-btn").addEventListener("click", closeClicked);
+document.querySelector("#book-submit-btn").addEventListener("click", submitClicked);
+resetInput();
+renderBooks();
--- scripts/libary.js
+++ scripts/libary.js
@@ -12,7 +12,8 @@ export function Book(title, author, published, numberOfPages, haveRead = false) 
 Book.prototype.info = () => `${this.title} by ${this.author}, ${this.numberOfPages}, ${this.haveRead ? "already read" : "not read yet"}.`;
 
 myLibary.push(new Book("Der Stein der Weisen", "J. Rowling", 1998, 256));
-myLibary.push(new Book("The Lord of the Rings", "J. R. R. Tolkien", 1954, 543));
+myLibary.push(new Book("The Lord of the Rings", "J. R. R. Tolkien", 1954, 543, true));
 myLibary.push(new Book("Lord of the Flies", "William Golding", 1954, 224));
 myLibary.push(new Book("It", "S. King", 1986, 1138, true));
 myLibary.push(new Book("The Green Mile", "S. King", 1999, 544));
+myLibary.push(new Book("Der Steppenwolf", "H. Hesse", 1974, 277));
